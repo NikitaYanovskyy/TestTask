f0b78eb34f743e4dd7dbdc5042ba00696e351e47
Nikita Yanovskyy <nik.yanovskyy@gmail.com>
2021-04-07 18:00:31 +0300

    s

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
+const blackButton = document.querySelector('#black_btn');
+const whiteButton = document.querySelector('#white_btn');
+const homeDark = document.querySelector('.home_dark_block');
+const homeLight = document.querySelector('.home_light_block');
 
+
+homeLight.addEventListener('mouseover', (e)=>{
+    homeDark.style.clipPath = 'polygon(62% 0, 100% 0, 100% 100%, 58% 100%)'
+    blackButton.style.clipPath = 'polygon(3% 0, 100% 0, 97% 100%, 0% 100%)'
+    whiteButton.style.clipPath = 'polygon(3% 0, 3% 0, 0 100%, 0% 100%)'
+})
+homeDark.addEventListener('mouseover', (e)=>{
+    homeDark.style.clipPath = 'polygon(42% 0, 100% 0, 100% 100%, 38% 100%)'
+    whiteButton.style.clipPath = 'polygon(3% 0, 100% 0, 97% 100%, 0% 100%)'
+    blackButton.style.clipPath = 'polygon(3% 0, 3% 0, 0 100%, 0% 100%)'
+})
